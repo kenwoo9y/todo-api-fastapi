07e4ee8b17039fde772dc6e44f61c1eeb14134ed
kenwoo9y <kenwoo9y@gmail.com>
2024-02-09 23:37:07 +0900

    update router

--- api/routers/task.py
+++ api/routers/task.py
@@ -12,10 +12,10 @@ async def show():
 async def create(task_body: task_schema.TaskCreate):
     return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
-@router.put("/tasks/{id}")
-async def update():
-    pass
+@router.put("/tasks/{id}", response_model=task_schema.TaskCreateResponse)
+async def update(task_id: int, task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
 
-@router.delete("/tasks/{id}")
-async def delete():
-    pass
+@router.delete("/tasks/{id}", response_model=None)
+async def delete(task_id: int):
+    return
