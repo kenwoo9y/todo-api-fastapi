68a01474fdf2c86d0d70e055780732e48ad1f3b6
kenwoo9y <kenwoo9y@gmail.com>
2024-04-05 20:54:49 +0900

    fix user_crud

--- api/cruds/user_crud.py
+++ api/cruds/user_crud.py
@@ -1,15 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from typing import List
 
 import api.models.user_model as user_model
 import api.schemas.user_schema as user_schema
 
-async def create(db: AsyncSession, user: user_schema.UserCreate):
-    db_user = user_model.User(**user.dict())
-    db.add(db_user)
+async def create(db: AsyncSession, user_create: user_schema.UserCreate):
+    user = user_model.User(**user_create.dict())
+    db.add(user)
     
     await db.commit()
-    await db.refresh(db_user)
-    return db_user
+    await db.refresh(user)
+    return user
 
 async def get(db: AsyncSession, id: int):
     async with db.begin():
@@ -32,22 +35,22 @@ async def get_all(db: AsyncSession) -> List[user_model.User]:
         )
         return result.scalars().all()
 
-async def update(db: AsyncSession, id: int, user: user_schema.UserUpdate):
+async def update(db: AsyncSession, id: int, user_update: user_schema.UserUpdate):
     async with db.begin():
         db_user = await db.execute(
             select(user_model.User).where(user_model.User.id == id)
         )
-        target_user = db_user.scalars().first()
+        user = db_user.scalars().first()
         
-        if target_user:
-            for key, value in user.dict().items():
-                setattr(target_user, key, value)
+        if user:
+            for key, value in user_update.dict().items():
+                setattr(user, key, value)
             await db.commit()
             await db.refresh(user)
             
-        return target_user
+        return user
 
-async def delete_user(db: AsyncSession, id: int):
+async def delete(db: AsyncSession, id: int):
     async with db.begin():
         db_user = await db.execute(
             select(user_model.User).where(user_model.User.id == id)
