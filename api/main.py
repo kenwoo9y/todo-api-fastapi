20f474eb1653f730db773de86d6bda1516c5284a
kenwoo9y <kenwoo9y@gmail.com>
2024-04-07 23:52:28 +0900

    add user_router to main

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
-from api.routers import task_router
+from api.routers import task_router, user_router
 
 app = FastAPI()
 app.include_router(task_router.router)
+app.include_router(user_router.router)
