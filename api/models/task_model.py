c2dcca95c805ff1b36761f1902abd09c74273ce4
kenwoo9y <kenwoo9y@gmail.com>
2024-04-15 00:06:58 +0900

    Fixed back_populates mistake

--- api/models/task_model.py
+++ api/models/task_model.py
@@ -14,4 +14,4 @@ class Task(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
     
-    owner = relationship("User", back_populates="users")
+    owner = relationship("User", back_populates="tasks")
