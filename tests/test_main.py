7a9f084b898f2d9fd789630ff691edb01bc2a57b
kenwoo9y <kenwoo9y@gmail.com>
2024-04-24 23:35:46 +0900

    add test methods

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,9 @@
 import pytest
+import pytest_asyncio
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+import starlette.status
 
 from api.db import get_db, Base
 from api.main import app
@@ -9,7 +11,7 @@ from api.main import app
 ASYNC_DB_URL = "sqlite+aiosqlite:///:memory:"
 
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def async_client() -> AsyncClient:
     # Async用のengineとsessionを作成
     async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
@@ -32,3 +34,26 @@ async def async_client() -> AsyncClient:
     # テスト用に非同期HTTPクライアントを返却
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
+
+@pytest.mark.asyncio
+async def test_create_task_ok(async_client):
+    response = await async_client.post("/tasks", json={
+        "title": "テストタスク",
+        "description": "テストタスク",
+        "status": "waiting",
+        "owner_id": "9999"
+    })
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert response_obj["title"] == "テストタスク"
+    
+@pytest.mark.asyncio
+async def test_create_user_ok(async_client):
+    response = await async_client.post("/users", json={
+        "user_name": "foobar",
+        "email": "foobar@example.com",
+        "first_name": "foo",
+        "last_name": "bar"
+    })
+    response_obj = response.json()
+    assert response_obj["user_name"] == "foobar" 
