ca131436488cfd73e653ff96e0268a38aea1abfc
kenwoo9y <kenwoo9y@gmail.com>
2024-04-28 01:10:27 +0900

    Added postgresql connection information

--- api/db.py
+++ api/db.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 ASYNC_DB_URL = "mysql+aiomysql://root@mysql-db:3306/todo?charset=utf8"
+#ASYNC_DB_URL = "postgresql+asyncpg://todo:todo@postgresql-db:5432/todo"
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(
--- api/migrate_db.py
+++ api/migrate_db.py
@@ -4,6 +4,7 @@ from api.models.task_model import Base
 from api.models.user_model import Base
 
 DB_URL = "mysql+pymysql://root@mysql-db:3306/todo?charset=utf8"
+#DB_URL = "postgresql+psycopg2://todo:todo@postgresql-db:5432/todo"
 engine = create_engine(DB_URL, echo=True)
 
 
