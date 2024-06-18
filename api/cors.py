b284d2ba7665ce1afdd099904620d914c9ed1e12
kenwoo9y <kenwoo9y@gmail.com>
2024-06-18 23:48:19 +0900

    Add cors.py

---
+++ api/cors.py
@@ -0,0 +1,14 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    
+]
+
+def add_cors_middleware(app):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins= origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
--- api/main.py
+++ api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from api.routers import task_router, user_router
+from api.cors import add_cors_middleware
 
 app = FastAPI()
 app.include_router(task_router.router)
 app.include_router(user_router.router)
+add_cors_middleware(app=app)
