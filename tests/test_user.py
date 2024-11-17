3bbec4a10b97dfb7283c490a16977291b5678df8
kenwoo9y <kenwoo9y@gmail.com>
2024-11-17 22:48:47 +0900

    fix user test

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,13 +3,24 @@ import starlette.status
 
 @pytest.mark.asyncio
 async def test_create_user_ok(async_client):
-    response = await async_client.post("/users", json={
+    # リクエストデータ
+    payload = {
         "user_name": "foobar",
         "email": "foobar@example.com",
         "first_name": "foo",
         "last_name": "bar",
         "password": "password"
-    })
+    }
+
+    # ユーザー作成APIへのPOSTリクエスト
+    response = await async_client.post("/users", json=payload)
+    
+    # ステータスコードの確認
     assert response.status_code == starlette.status.HTTP_200_OK
+    
+    # レスポンスデータの確認
     response_obj = response.json()
-    assert response_obj["user_name"] == "foobar" 
+    assert response_obj["user_name"] == payload["user_name"]
+    assert response_obj["email"] == payload["email"]
+    assert response_obj["first_name"] == payload["first_name"]
+    assert response_obj["last_name"] == payload["last_name"]
