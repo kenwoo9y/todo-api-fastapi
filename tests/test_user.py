464d51811766024f6c5b3e92f3d5b6d9648850c4
kenwoo9y <kenwoo9y@gmail.com>
2024-11-18 19:55:30 +0900

    add test_get_user_by_id

--- tests/test_user.py
+++ tests/test_user.py
@@ -24,3 +24,25 @@ async def test_create_user_ok(async_client):
     assert response_obj["email"] == payload["email"]
     assert response_obj["first_name"] == payload["first_name"]
     assert response_obj["last_name"] == payload["last_name"]
+
+@pytest.mark.asyncio
+async def test_get_user_by_id(async_client):
+    # 新規ユーザーの作成
+    payload = {
+        "user_name": "testuser",
+        "email": "test@example.com",
+        "first_name": "test",
+        "last_name": "user",
+        "password": "password"
+    }
+    
+    create_response = await async_client.post("/users", json=payload)
+    user_id = create_response.json()["id"]
+    
+    # GETリクエストでユーザー取得
+    response = await async_client.get(f"/users/{user_id}")
+    assert response.status_code == starlette.status.HTTP_200_OK
+    user_data = response.json()
+    assert user_data["user_name"] == payload["user_name"]
+    assert user_data["email"] == payload["email"]
+
