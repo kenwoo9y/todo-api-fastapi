5d0ebb0c4ca3ff93d16e731d88ab27142943850b
kenwoo9y <kenwoo9y@gmail.com>
2024-11-25 21:29:01 +0900

    fix F811 linter error

--- api/migrate_db.py
+++ api/migrate_db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
-from api.models.task_model import Base
-from api.models.user_model import Base
+from api.models.task_model import Base as TaskBase
+from api.models.user_model import Base as UserBase
 
 DB_URL = "mysql+pymysql://root@mysql-db:3306/todo?charset=utf8"
 # DB_URL = "postgresql+psycopg2://todo:todo@postgresql-db:5432/todo"
@@ -9,8 +9,10 @@ engine = create_engine(DB_URL, echo=True)
 
 
 def reset_database():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+    TaskBase.metadata.drop_all(bind=engine)
+    UserBase.metadata.drop_all(bind=engine)
+    TaskBase.metadata.create_all(bind=engine)
+    UserBase.metadata.create_all(bind=engine)
 
 
 if __name__ == "__main__":
