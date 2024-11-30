2a56864e303817111bb74e7976e48b00443d1b57
kenwoo9y <kenwoo9y@gmail.com>
2024-11-30 21:35:53 +0900

    fix user router

--- api/routers/user_router.py
+++ api/routers/user_router.py
@@ -13,32 +13,36 @@ from api.db import get_db
 router = APIRouter()
 
 
-@router.post("/users", response_model=user_schema.UserResponse)
-async def create(body: user_schema.UserCreate, db: AsyncSession = Depends(get_db)):
+@router.post("/users", response_model=user_schema.UserResponse, status_code=201)
+async def create_user(body: user_schema.UserCreate, db: AsyncSession = Depends(get_db)):
     user = await user_crud.create(db=db, user_create=body)
     return user
 
 
 @router.get("/users/{id}", response_model=Optional[user_schema.UserResponse])
-async def get(id: int, db: AsyncSession = Depends(get_db)):
+async def get_user_by_id(id: int, db: AsyncSession = Depends(get_db)):
     user = await user_crud.get(db=db, id=id)
+    if user is None:
+        raise HTTPException(status_cod=404, detail="User not found")
     return user
 
 
 @router.get("/users/username/{username}", response_model=Optional[user_schema.UserResponse])
-async def get_by_username(username: str, db: AsyncSession = Depends(get_db)):
+async def get_user_by_username(username: str, db: AsyncSession = Depends(get_db)):
     user = await user_crud.get_by_username(db=db, username=username)
+    if user is None:
+        raise HTTPException(status_cod=404, detail="User not found")
     return user
 
 
 @router.get("/users", response_model=List[user_schema.UserResponse], response_model_exclude_unset=True)
-async def get_all(db: AsyncSession = Depends(get_db)):
+async def get_all_users(db: AsyncSession = Depends(get_db)):
     users = await user_crud.get_all(db=db)
     return users
 
 
 @router.put("/users/{id}", response_model=user_schema.UserResponse)
-async def update(id: int, body: user_schema.UserUpdate, db: AsyncSession = Depends(get_db)):
+async def update_user(id: int, body: user_schema.UserUpdate, db: AsyncSession = Depends(get_db)):
     user = await user_crud.get(db=db, id=id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -47,10 +51,10 @@ async def update(id: int, body: user_schema.UserUpdate, db: AsyncSession = Depen
     return updated_user
 
 
-@router.delete("/users/{id}", response_model=None)
-async def delete(id: int, db: AsyncSession = Depends(get_db)):
+@router.delete("/users/{id}", status_code=204)
+async def delete_user(id: int, db: AsyncSession = Depends(get_db)):
     user = await user_crud.get(db=db, id=id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
-    return await user_crud.delete(db=db, user=user)
+    await user_crud.delete(db=db, user=user)
